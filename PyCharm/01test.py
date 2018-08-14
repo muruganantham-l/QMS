36a81b57d925f5922dfa1712430fc5434ce02a96
Muruganantham <muruganplsql@gmail.com>
2018-08-14 15:50:09 +0800

    for studying python

--- PyCharm/01test.py
+++ PyCharm/01test.py
@@ -1,4 +1,21 @@
-name = "test\x10"
+a = ['test',10,20L,30]
+print a
+print a[2]
 
-print name
- 
+print a[-1]
+
+print a[1:]
+
+print a[3]
+
+del a[2]
+
+print a
+
+a.append('murugan')
+
+print a
+
+a.insert(3,'ok ok')
+
+print a
